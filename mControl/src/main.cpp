2f16a3230b7930ff3899b0355eaa2d636b4a8297
bhanuka kamantha <50150022+bkamantha@users.noreply.github.com>
2023-11-15 22:32:39 +0530

    automatic motor direction

--- mControl/src/main.cpp
+++ mControl/src/main.cpp
@@ -3,44 +3,52 @@
 const int dirPin = 3;
 const int stepPin = 2;
 
-const int stepsPerRevolution = 200;  // Adjust for your motor's steps per revolution
+const int stepsPerRevolution = 200; // Adjust for your motor's steps per revolution
 
-void setup() {
+void setup()
+{
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   Serial.begin(9600);
 }
 
-void moveStepper(int steps, int direction, int initialDelayMicros) {
+void moveStepper(int steps, int direction, int initialDelayMicros)
+{
   digitalWrite(dirPin, direction);
 
   int currentDelayMicros = initialDelayMicros;
 
-  for (int x = 0; x < steps; x++) {
+  for (int x = 0; x < steps; x++)
+  {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(currentDelayMicros);
     digitalWrite(stepPin, LOW);
     delayMicroseconds(currentDelayMicros);
 
     // Gradually decrease delay for smoother acceleration
-    if (currentDelayMicros > 500) {
+    if (currentDelayMicros > 500)
+    {
       currentDelayMicros -= 10;
     }
   }
 }
 
-void loop() {
+void loop()
+{
   // Adjust the speed and number of rounds as needed
   int desiredSpeed = 20;  // Adjust the desired speed in steps per second
-  int numberOfRounds = 5;  // Adjust the number of rounds to move the motor
+  int numberOfRounds = 5; // Adjust the number of rounds to move the motor
+  bool Direction = true;
 
   int stepsToMove = numberOfRounds * stepsPerRevolution;
-  int initialDelayMicros = 1000;  // Adjust the initial delay as needed
+  int initialDelayMicros = 1000; // Adjust the initial delay as needed
 
   // Move the stepper motor with specified speed and number of rounds
-  moveStepper(stepsToMove, HIGH, initialDelayMicros);
+  moveStepper(stepsToMove, Direction ? HIGH : LOW, initialDelayMicros);
+
+  Direction = !Direction;
 
   // Stop the motor
   digitalWrite(stepPin, LOW);
-  delay(2500);  // Adjust the delay as needed
+  delay(2500); // Adjust the delay as needed
 }
